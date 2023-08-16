6a2cf006b22f28d99a2fb94676a348a3232ac3db
safouat <120058233+safouat@users.noreply.github.com>
2023-08-17 00:41:29 +0200

    Delete ressetpassword.blade.php

--- resources/views/annonces/ressetpassword.blade.php
+++
@@ -1,9 +0,0 @@
-<form method="post" action="{{route('password.update')}}">
-    @csrf
-<label>Email:</label>
-<input type="text" name="email" ><br>
-<label>password:</label>
-<input type="text" name="password" ><br>
-
-<button type="submit">Send</button>
-</form>
